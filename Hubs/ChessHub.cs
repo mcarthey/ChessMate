62c01f9f7e16f140549b702e583c59e532a85b00
Mark McArthey <mcarthey@gmail.com>
2024-12-14 14:50:55 -0600

    Add project files.

---
+++ Hubs/ChessHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChessMate.Hubs
+{
+    public class ChessHub : Hub
+    {
+        public async Task SendMove(string start, string end)
+        {
+            Console.WriteLine($"Move received: {start} -> {end}");
+            await Clients.All.SendAsync("ReceiveMove", start, end);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using ChessMate.Data;
+using ChessMate.Services;
+using Microsoft.AspNetCore.SignalR;
+using ChessMate.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+// Register your custom ChessService
+builder.Services.AddSingleton<ChessService>();
+
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapHub<ChessHub>("/chesshub"); // SignalR hub for the chess app
+app.MapFallbackToPage("/_Host");
+
+app.Run();
